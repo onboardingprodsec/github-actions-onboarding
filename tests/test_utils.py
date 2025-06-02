6669aac17387461358e3aac2cc54dbbbe89bbc44
GehadSalemFekry <GehadSalemFekry@aucegypt.edu>
2025-06-02 12:28:02 +0300

    Add more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,6 @@ from src import utils
 class TestUtils(unittest.TestCase):
     def test_add(self):
         self.assertEqual(utils.add(1, 2), 3)
+
+    def test_subtract(self):
+        self.assertEqual(utils.subtract(5, 3), 2)
