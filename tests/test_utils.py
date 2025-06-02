274a924f3d74ca591748a4986e2c3fa88c1decaa
GehadSalemFekry <GehadSalemFekry@aucegypt.edu>
2025-06-02 12:53:38 +0300

    Add one more test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,3 +11,8 @@ class TestUtils(unittest.TestCase):
 
     def test_multiply(self):
         self.assertEqual(utils.multiply(2, 3), 6)
+
+    def test_divide(self):
+        self.assertEqual(utils.divide(6, 2), 3)
+        with self.assertRaises(ValueError):
+            utils.divide(5, 0)
