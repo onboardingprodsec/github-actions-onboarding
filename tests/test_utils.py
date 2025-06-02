68e5bcdf80652cbcddacea6fb3cc7f293cfd107a
GehadSalemFekry <GehadSalemFekry@aucegypt.edu>
2025-06-02 12:37:03 +0300

    Add one more test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,3 +8,6 @@ class TestUtils(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(utils.subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(utils.multiply(2, 3), 6)
